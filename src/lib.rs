45142b0a62bf3136992b30fea625c7884c9c40f4
Sam Delmerico <svdelmerico@gmail.com>
2019-12-28 20:40:56 -0500

    added movement

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,7 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, galaga!");
-}
-
-#[wasm_bindgen]
-#[repr(u32)]
+#[repr(i32)]
 #[derive(Clone)]
 pub enum EntityType {
     Ship = 0,
@@ -29,30 +24,34 @@ pub enum EntityType {
 #[derive(Clone)]
 pub struct Entity {
     kind: EntityType,
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
     draw: bool
 }
 
 #[wasm_bindgen]
 impl Entity {
-    pub fn get_x(&self) -> u32 {
+    pub fn get_x(&self) -> i32 {
         self.x
     }
 
-    pub fn get_y(&self) -> u32 {
+    pub fn get_y(&self) -> i32 {
         self.y
     }
 
-    pub fn set_x(&mut self, x: u32) {
-        self.x = x % 20
+    pub fn set_x(&mut self, x: i32) {
+        self.x = x
+    }
+
+    pub fn set_y(&mut self, y: i32) {
+        self.y = y
     }
 }
 
 #[wasm_bindgen]
 pub struct Universe {
+    width:  u32,
     height: u32,
-    width: u32,
     entities: Vec<Entity>
 }
 
@@ -60,8 +59,8 @@ pub struct Universe {
 impl Universe {
     pub fn new() -> Universe {
         let mut u = Universe {
+            width:  20,
             height: 20,
-            width: 20,
             entities: vec![]
         };
 
@@ -73,11 +72,16 @@ impl Universe {
         self.entities.push(Entity { kind: EntityType::Ship, x: 0, y: 0, draw: true })
     }
 
+    fn get_ship_mut(&mut self) -> Option<&mut Entity> {
+        self.entities.get_mut(0)
+    }
+
     pub fn tick(&mut self) {
-        if let Some(ship) = self.entities.get_mut(0) {
-            let x = ship.get_x();
-            ship.set_x(x + 1 % self.height)
-        }
+        //let width = self.width;
+        //if let Some(ship) = self.get_ship_mut() {
+            //let x = ship.get_x();
+            //ship.set_x(x + 1 % width)
+        //}
     }
 
     pub fn width(&self) -> u32 {
@@ -94,5 +98,34 @@ impl Universe {
 
     pub fn n_entities(&self) -> usize {
         self.entities.len()
+    }
+
+    pub fn left_arrow_key(&mut self) {
+        let width = self.width;
+        if let Some(ship) = self.get_ship_mut() {
+            let x = ship.get_x();
+            ship.set_x((x - 1).rem_euclid(width as i32));
+        }
+    }
+    pub fn right_arrow_key(&mut self) {
+        let width = self.width;
+        if let Some(ship) = self.get_ship_mut() {
+            let x = ship.get_x();
+            ship.set_x((x + 1) % width as i32)
+        }
+    }
+    pub fn up_arrow_key(&mut self) {
+        let height = self.height;
+        if let Some(ship) = self.get_ship_mut() {
+            let y = ship.get_y();
+            ship.set_y((y + 1) % height as i32)
+        }
+    }
+    pub fn down_arrow_key(&mut self) {
+        let height = self.height;
+        if let Some(ship) = self.get_ship_mut() {
+            let y = ship.get_y();
+            ship.set_y((y - 1).rem_euclid(height as i32));
+        }
     }
 }
